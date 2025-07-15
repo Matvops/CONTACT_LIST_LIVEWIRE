673ed5e2635df99abe1022d3d0d9ff2ea55c74cb
Matheus <matheusmartinscia@gmail.com>
2025-07-15 18:52:07 -0300

    Feat: Implementa listagem de contatos

--- resources/views/livewire/contact.blade.php
+++ resources/views/livewire/contact.blade.php
@@ -1,4 +1,20 @@
 <div class="card p-5">
-    <p>Contacts</p>
-    <hr class="m-0 p-0">
+
+    <p class="mb-3">Contacts</p>
+
+    @if ($contacts->count() === 0)
+        <div class="opacity-50">
+            No contacts found
+        </div>
+    @else
+        @foreach($contacts as $contact)
+            <div class="card p-3 mb-1 bg-dark">
+                <div class="row">
+                    <div class="col">Name: {{ $contact->nome }}</div>
+                    <div class="col">Email: {{ $contact->email }}</div>
+                    <div class="col">Phone: {{ $contact->phone }}</div>
+                </div>
+            </div>
+        @endforeach
+    @endif
 </div>
